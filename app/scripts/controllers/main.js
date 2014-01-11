36b0c1e5b8e1f6aef0d0c910683a2a66fb1550ae
Tanathip Viriya <tanathip.se@gmail.com>
2014-01-11 10:51:03 +0700

    initial project

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('geekBattleApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
