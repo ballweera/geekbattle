b933f4e27ef7ffc21a44210dd77cc2ab05a52e69
Tanathip Viriya <tanathip.se@gmail.com>
2014-01-11 13:16:19 +0700

    JsonReader service

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,18 +1,13 @@
 'use strict';
 
 angular.module('geekBattleApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope,JsonReader) {
 
     $scope.teams = ['kitty', 'vanz', 'pikaju'];
     $scope.team1List = [];
 
     $scope.updateTeam1PlayerList = function(){
-    	$scope.team1List = ['','',''];
+    	$scope.team1List = ['A','B','C'];
     };
 
   });
---
+++ app/scripts/services/JsonReader.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('geekBattleApp')
+	.factory('JsonReader', function() {
+		var getPlayerData = function(){
+			return 1;
+		};
+  		return {
+  			getPlayerData : getPlayerData
+  		}
+});
